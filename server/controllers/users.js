ac3c6aaca4c665757c45ceeed18ca2eb1bcb69c3
tombroskipc <tombroskipc@gmail.com>
2022-02-18 00:59:00 +0700

    finish 2.2.1 getUserByName

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,15 +1,44 @@
 import User from "../models/users.js";
 
 export const getUsers = async (req, res) => {
-    // const { username,  } = req.query
-    try {
-        const users = await User.find().select('-__v');
-        console.log("Get Users success: ", users);
-
-        res.status(200).json(users);
+    const { name, username, email } = req.query
+    console.log(`name: ${name}, username: ${username}, email: ${email}`)
+    if (name || username || email) {
+        try {
+            const selectUser = {
+                $or:
+                    [
+                        {
+                            username: {
+                                $regex: `.*${name}.*`,
+                                $options: 'i'
+                            }
+                        },
+                        {
+                            email: {
+                                $regex: `.*${name}.*`,
+                                $options: 'i'
+                            }
+                        }
+                    ]
+            }
+            const queryResponse = await User.find().select('-__v')
+            console.log("Get Query success: ", queryResponse);
+            res.status(200).json(queryResponse);
+        } catch (error) {
+            console.log(error.message)
+        }
     }
-    catch (error) {
-        res.status(500).json({ message: error.message });
+    else {
+        try {
+            const users = await User.find().select('-__v');
+            console.log("Get Users success: ", users);
+
+            res.status(200).json(users);
+        }
+        catch (error) {
+            res.status(500).json({ message: error.message });
+        }
     }
 }
 
